87d3922a96441ed86a224c64f899875a713b456a
Rohit000 <rohit@crowdvolley.com>
2026-02-19 10:05:09 +0530

    change 1

--- frontend/frontend/public/embed.js
+++ frontend/frontend/public/embed.js
@@ -9,13 +9,13 @@
   const iframe = document.createElement("iframe");
   // iframe.src = "http://localhost:5173"; // test URL, replace later
   iframe.src = "https://frontend-a7v3.onrender.com/";
-  // iframe.style.position = "fixed";
-  // iframe.style.bottom = "0";
-  // iframe.style.left = "0px";
-  // iframe.style.width = "100%";
-  // iframe.style.height = "100%";
-  // iframe.style.zIndex = "9999";
-  // iframe.style.border = "none";
+  iframe.style.position = "fixed";
+  iframe.style.bottom = "0";
+  iframe.style.left = "0px";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.zIndex = "9999";
+  iframe.style.border = "none";
   chatContainer.appendChild(iframe);
   document.body.appendChild(chatContainer);
 })();
